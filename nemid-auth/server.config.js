b0ec8aabb5fa96be0f76d52bd00c499deb4fc9b4
MariusMM2 <dmunteanu91@gmail.com>
2021-01-11 14:07:25 +0100

    removed commented config lines

--- nemid-auth/server.config.js
+++ nemid-auth/server.config.js
@@ -3,9 +3,6 @@ const config = {}
 config.port = process.env.PORT || 3001;
 config.dbName = "nemid-auth.sqlite";
 config.dbLocation = `../db/${config.dbName}`;
-// config.nemidApiDomain = process.env.DB_DOMAIN || "localhost";
-// config.nemidApiDomain = process.env.DB_DOMAIN || "localhost";
-// config.nemidApiPort = 3000;
 config.nemidApiString = process.env.API_STRING || "http://localhost:3000";
 
 config.dbState = {
