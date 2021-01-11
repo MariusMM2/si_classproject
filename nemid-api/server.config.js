a6ce151125a0578531179de45407cfe1663f2ad1
MariusMM2 <dmunteanu91@gmail.com>
2021-01-11 03:36:11 +0100

    renamed db file from nemid to nemid-api

--- nemid-api/server.config.js
+++ nemid-api/server.config.js
@@ -1,7 +1,7 @@
 const config = {}
 
 config.port = process.env.PORT || 3000;
-config.dbName = "nemid.sqlite";
+config.dbName = "nemid-api.sqlite";
 config.dbLocation = `../db/${config.dbName}`;
 
 config.locale = 'da-DK';
