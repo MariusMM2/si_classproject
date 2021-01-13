53b6e41b8eb9ce2978f502e9116ff70491ebfb77
MariusMM2 <dmunteanu91@gmail.com>
2021-01-13 20:28:16 +0100

    fixed db name

--- main-system/server.config.js
+++ main-system/server.config.js
@@ -1,7 +1,7 @@
 const config = {}
 
 config.port = process.env.PORT || 3000;
-config.dbName = "tokenDb.sqlite";
+config.dbName = "userDb.sqlite";
 config.dbLocation = `../db/${config.dbName}`;
 config.nemidAuthString = process.env.AUTH_STRING || "http://localhost:3001";
 config.nemidApiString = process.env.API_STRING || "http://localhost:3000";
